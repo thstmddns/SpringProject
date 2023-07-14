7206a6ed71279e285ec269245556dd2772c1148b
thstmddns <thstmddns@naver.com>
2023-07-14 17:45:48 +0900

    두번째 수정 작업 thstmddns

--- spring01/src/spring01/sp01.java
+++ spring01/src/spring01/sp01.java
@@ -4,6 +4,6 @@ public class sp01 {
 
 	public static void main(String[] args) {
 		System.out.println("첫번째 작업파일");
-
+		System.out.println("두번째 작업파일");
 	}
 }
---
+++ spring01/src/spring01/sp02.java
@@ -0,0 +1,10 @@
+package spring01;
+
+public class sp02 {
+
+	public static void main(String[] args) {
+		System.out.println("새로만든 파일");
+
+	}
+
+}
